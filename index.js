0e0e9979ae59aeafe56ec579e0a7010e1396acf6
fejesgergorichard <50084782+fejesgergorichard@users.noreply.github.com>
2023-05-14 16:07:58 +0200

    changed background image to random green one

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=green")
     .then(res => res.json())
     .then(data => {  
-        document.body.style.backgroundImage = `url(background.jpg)`
+        document.body.style.backgroundImage = data.urls.full
         document.getElementById("author").innerText = `By: ${data.user.name}`
     })
     .catch(err => {
